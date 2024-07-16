0f738ca139a5663ff1c8fe74713e583621abdfcf
NduduzoGatsheni <nduduzondlovu635@gmail.com>
2024-07-16 09:58:58 +0200

    profile

--- src/app/profile/tab3.page.ts
+++ src/app/profile/tab3.page.ts
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-
+  studentNumber: string = '123456';
+  studentName: string = 'John';
+  studentSurname: string = 'Doe';
+  fromTime: string = '08:00 AM';
+  toTime: string = '03:00 PM';
+  message: string = 'Welcome to the new semester!';
   constructor() {}
 
 }
